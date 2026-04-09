c37c88f8ed758691760b0498f7c8ced6d22f2ed1
Jared Delgado <jaredruben.delgado@cetys.edu.mx>
2026-04-08 17:33:01 -0700

    Clase 08 de Abril (3)

--- src/main/java/com/walmartapi/mapper/impl/ProductMapper.java
+++ src/main/java/com/walmartapi/mapper/impl/ProductMapper.java
@@ -3,15 +3,26 @@ package com.walmartapi.mapper.impl;
 import com.walmartapi.entity.ProductEntity;
 import com.walmartapi.mapper.CustomObjectMapper;
 import com.walmartapi.model.Product;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductMapper implements CustomObjectMapper<ProductEntity, Product> {
     @Override
     public ProductEntity mapToEntity(Product dto) {
-        return null;
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setDescription(dto.getDescription());
+        productEntity.setPrice(dto.getPrice());
+        productEntity.setName(dto.getName());
+        return productEntity;
     }
 
     @Override
     public Product mapToDto(ProductEntity dto) {
-        return null;
+        Product savedProduct = new Product();
+        savedProduct.setName(dto.getName());
+        savedProduct.setDescription(dto.getDescription());
+        savedProduct.setPrice(dto.getPrice());
+        savedProduct.setId(dto.getId());
+        return savedProduct;
     }
 }
--- src/main/java/com/walmartapi/service/ProductService.java
+++ src/main/java/com/walmartapi/service/ProductService.java
@@ -1,41 +1,40 @@
 package com.walmartapi.service;
 
 import com.walmartapi.entity.ProductEntity;
+import com.walmartapi.exception.NotFound;
+import com.walmartapi.mapper.CustomObjectMapper;
 import com.walmartapi.model.Product;
 import com.walmartapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CustomObjectMapper<ProductEntity, Product> productMapper;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CustomObjectMapper<ProductEntity, Product> productMapper) {
         this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
     public Product saveProduct(Product product) {
 
-        // Product -> Entity
-        ProductEntity productEntity = new ProductEntity();
-        productEntity.setDescription(product.getDescription());
-        productEntity.setPrice(product.getPrice());
-        productEntity.setName(product.getName());
+        ProductEntity newProduct = productMapper.mapToEntity(product);
+        ProductEntity savedEntity = productRepository.save(newProduct);
 
-        ProductEntity savedEntity = productRepository.save(productEntity);
+        return productMapper.mapToDto(savedEntity);
+    }
 
-        Product savedProduct = new Product();
+    public Product getProductById(Long id) {
+        Optional<ProductEntity> product = productRepository.findById(id);
 
-        // Entity ->
-        savedProduct.setName(savedEntity.getName());
-        savedProduct.setDescription(savedEntity.getDescription());
-        savedProduct.setPrice(savedEntity.getPrice());
-        savedProduct.setId(savedEntity.getId());
+        if(product.isEmpty()) {
+            throw new NotFound("Product not found");
+        }
 
-        // map POJO to entity
-        // Call DB
-        // map entity to POJO
-
-        return product;
+        return productMapper.mapToDto(product.get());
     }
 
 }
